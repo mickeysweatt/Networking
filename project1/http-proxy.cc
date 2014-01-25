a537fa43df7dd764f712cbb2ef0d3474de430916
Mickey Sweatt <mickeysweatt@engineering.ucla.edu>
2014-01-25 01:26:24 -0800

    Wrapped all the server functionality in a nice OO wrapper

--- project1/http-proxy.cc
+++ project1/http-proxy.cc
@@ -14,6 +14,7 @@
 #include <arpa/inet.h>
 #include <sys/wait.h>
 #include <signal.h>
+#include "http-server.h"
 
 #define PORT "14886"  // the port users will be connecting to
 
@@ -21,107 +22,19 @@
 
 #define BUFFER_SIZE 4096
 
+// set up the signal handle to reap all dead processes
+#define set_up_signal_handler() \
+    sa.sa_handler = sigchld_handler;\
+    sigemptyset(&sa.sa_mask); \
+    sa.sa_flags = SA_RESTART; \
+    if (sigaction(SIGCHLD, &sa, NULL) == -1) { \
+        perror("sigaction");        \
+        exit(1); \
+    }
+
 void sigchld_handler(int s)
 {
     while(waitpid(-1, NULL, WNOHANG) > 0);
-}
-
-// get sockaddr, IPv4 or IPv6:
-void *get_in_addr(struct sockaddr *sa)
-{
-    if (sa->sa_family == AF_INET) {
-        return &(((struct sockaddr_in*)sa)->sin_addr);
-    }
-
-    return &(((struct sockaddr_in6*)sa)->sin6_addr);
-}
-
-int init_server_socket()
-{
-    int sockfd;  // listen on sock_fd
-    struct addrinfo hints, *servinfo, *p;
-    int yes=1;
-    int rv;
-    
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM; // TCP
-    hints.ai_flags = AI_PASSIVE; // use my IP
-    
-    if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-        return -1;
-    }
-
-    // loop through all the results and bind to the first we can
-    for(p = servinfo; p != NULL; p = p->ai_next) {
-        if ((sockfd = socket(p->ai_family, p->ai_socktype,
-                p->ai_protocol)) == -1) {
-            perror("server: socket");
-            continue;
-        }
-
-        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
-                sizeof(int)) == -1) {
-            perror("setsockopt");
-            exit(1);
-        }
-
-        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-            close(sockfd);
-            perror("server: bind");
-            continue;
-        }
-        break;
-    }
-
-    if (p == NULL)  {
-        fprintf(stderr, "server: failed to bind\n");
-        return -2;
-    }
-
-    freeaddrinfo(servinfo); // all done with this structure
-    return sockfd;
-}
-
-int accept_connection(const int sockfd)
-{
-    socklen_t sin_size;
-    struct sockaddr_storage their_addr; // connector's address information
-    int new_fd; // new connection on new_fd
-    char s[INET6_ADDRSTRLEN];
-    ssize_t response_size;
-    char buff [BUFFER_SIZE];
-    
-    while(1) {  
-    // main accept() loop
-        sin_size = sizeof their_addr;
-        new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
-        if (new_fd == -1) {
-            perror("accept");
-            continue;
-        }
-
-        inet_ntop(their_addr.ss_family,
-            get_in_addr((struct sockaddr *)&their_addr),
-            s, sizeof s);
-        printf("server: got connection from %s\n", s);
-
-        if (!fork()) { // this is the child process
-            close(sockfd); // child doesn't need the listener
-            
-            // this is where we actually get the request from the server and 
-            // start to try to handle it
-            if ((response_size = recv(new_fd, buff, BUFFER_SIZE, 0)) == -1)
-            {
-                perror("recv");
-            }
-            
-            close(new_fd);
-            exit(0);
-        }
-        close(new_fd);  // parent doesn't need this
-    }
 }
 
 int main(void)
@@ -129,26 +42,18 @@ int main(void)
     int sockfd;
     struct sigaction sa;
     char s[INET6_ADDRSTRLEN];
-
-    sockfd = init_server_socket();
-
-    if (listen(sockfd, BACKLOG) == -1) {
-        perror("listen");
+    HTTPServer server;
+    if (server.getStatus() < 0)
+    {
         exit(1);
     }
-    
-    // set up the signal handle to reap all dead processes
-    sa.sa_handler = sigchld_handler;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = SA_RESTART;
-    
-    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
-        perror("sigaction");
-        exit(1);
-    }
-    
+    sockfd = server.getSocketFileDescriptor();
+    server.waitForConnection();
+    set_up_signal_handler();
     printf("server: waiting for connections...\n");
-    accept_connection(sockfd);
-
+    while (1) {
+        server.acceptConnection();
+    }
+    
     return 0;
 }
