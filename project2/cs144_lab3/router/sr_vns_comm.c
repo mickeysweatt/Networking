43cb7686ef3f6c951cb2dc055377aecef5d5c543
Mickey Sweatt <mickeysweatt@engineering.ucla.edu>
2014-03-09 14:16:51 -0700

    Merged with Rohan's code (not integrated)

--- project2/cs144_lab3/router/sr_vns_comm.c
+++ project2/cs144_lab3/router/sr_vns_comm.c
@@ -485,7 +485,8 @@ int sr_read_from_server_expect(struct sr_instance* sr /* borrowed */, int expect
                 fprintf(stderr,"Routing table not consistent with hardware\n");
                 return -1;
             }
-            Debug(" <-- Ready to process packets --> \n");
+            printf(" <-- Ready to process packets --> \n");
+            fflush(stdout);
             break;
 
             /* ---------------- VNS_RTABLE ---------------- */
@@ -582,11 +583,6 @@ int sr_send_packet(struct sr_instance* sr /* borrowed */,
                    unsigned int len,
                    const char* iface /* borrowed */)
 {
-    if (DEBUG)
-    {   Debug("Trying to send:\n");
-        print_hdrs(buf, len);
-    }
-	
 	c_packet_header *sr_pkt;
     unsigned int total_len =  len + (sizeof(c_packet_header));
 
